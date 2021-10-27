07e08ccf5ffc3821413098fdebb9bcc83a5e1a72
Klaudiusz <florekklaudiusz@gmail.com>
2021-10-27 21:47:14 +0200

    final commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import FeedbackForm from './components/FeedbackForm.svelte';
+	import FeedbackList from './components/FeedbackList.svelte';
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+	let feedback = [
+		{
+			id:  1,
+			rating: 10,
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nunc felis, molestie eu lacus quis, placerat volutpat mauris. Donec feugiat urna eget enim viverra sodales. Mauris eu massa at libero dignissim gravida non at est. Proin quis facilisis leo."
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: "Sed sagittis vestibulum lobortis. In nisl nibh, tincidunt quis lorem eget, auctor auctor leo. Ut at scelerisque erat, a pulvinar purus. Sed vel lacinia est. Sed ut laoreet neque. Quisque faucibus iaculis nunc, vitae scelerisque urna consequat vel."
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: "Nulla et purus sed odio dignissim ornare vitae quis orci. Suspendisse elit lacus, aliquam vitae commodo eu, dapibus et mi. Sed ex neque, iaculis nec velit vitae, vestibulum vulputate massa. Nullam id luctus odio. Integer gravida at ligula ut dapibus."
+		}
+	]
+
+	$: count = feedback.length;
+	$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length;
+
+	const addFeedback = (e) => {
+		const newFeedback = e.detail;
+		feedback = [newFeedback, ...feedback]
+	}
+
+	const deleteFeedback = (e) => {
+		const itemId = e.detail;
+		feedback = feedback.filter((item) =>  item.id != itemId)
+	}
+
+</script>
+
+<main class="container">
+	<FeedbackForm on:add-feedback={addFeedback}/>
+	<FeedbackStats {count} {average} />
+	<FeedbackList {feedback} on:delete-fedback={deleteFeedback}/>
+</main>
+
+<style>
+
+</style>
---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,59 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+    import Card from "./Card.svelte";
+    export let item;
+
+    const dispatch = createEventDispatcher();
+
+    const handleDelete = (itemId) => {
+        dispatch('delete-fedback', itemId)
+    }
+
+</script>
+
+<Card>
+<div class="num-display">
+    {item.rating}
+</div>
+<button class="close" on:click={() => handleDelete(item.id)}>
+    <i class="fas fa-window-close"></i>
+</button>
+<div class="text-display">
+    {item.text}
+</div>
+</Card>
+
+<style>
+    .num-display {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: -25px;
+        left: -25px;
+        width: 50px;
+        height: 50px;
+        background: #ff6a95;
+        color: #fff;
+        border: 2px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        text-align: center;
+        font-size: 20px;
+    }
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        cursor: pointer;
+        color: #ff6a95;
+        font-size: 1.4rem;
+        opacity: 0.5;
+        transition: .2s;
+        background: none;
+        border: none;
+    }
+    .close:hover {
+        opacity: 1;
+    }
+</style>
